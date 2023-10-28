89922df88c1f28847d353df5e2eed025575f6b51
Rima Mousavi <RimaMSV@outlook.com>
2023-10-28 14:07:43 +0330

    Starting game

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 const modalEl = document.querySelector("#modal");
-const openModalEl = document.querySelector('.open');
+const boardEl = document.querySelector("#wrapper");
+const cellEl = document.querySelectorAll("[data-cell]");
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
+let circleTurn
 
-openModalEl.addEventListener("click", () => {
-    modalEl.showModal();
-})
+startGame();
+
+function startGame() {
+    circleTurn = false;
+
+    cellEl.forEach(cell => {
+        cell.addEventListener("click", handleClick, { once: true })
+    })
+    setBoardHoverrClass()
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    //Check for Win
+    //Check for Draw
+    //Switch Turns
+    swapTurns();
+    setBoardHoverrClass();
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn;
+}
+
+function setBoardHoverrClass() {
+    boardEl.classList.remove(X_CLASS);
+    boardEl.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        boardEl.classList.add(CIRCLE_CLASS)
+    } else {
+        boardEl.classList.add(X_CLASS)
+    }
+}
